0417e903acb8b68f1508335db3fb373b1b5b8130
Andrew Domingues <contato@drewdomi.com>
2024-07-23 17:22:03 -0300

    feat: add in18 layout, header, about, sidemeu

---
+++ src/i18n/ui.ts
@@ -0,0 +1,54 @@
+export const languages = {
+  en: 'English',
+  'pt-br': 'Português',
+}
+
+export const defaultLang = 'pt-br'
+export const showDefaultLang = false
+
+export const LINKS = {
+  home: '/',
+  projects: '/projects',
+  blog: '/blog',
+  contato: '/#contact',
+  favicon: '/favicon.svg',
+}
+
+export const ui = {
+  'pt-br': {
+    'site.title': 'Andrew Domingues',
+    'site.desc': 'Andrew Domingues portfólio',
+    'site.keywords':
+      'drewdomi, Drew Domi, Programação, Programação WEB, Sites, Sistemas Web, Cruz Vermelha Portguesa, Cruz Vermelha de Amares',
+    'nav.home': 'Início',
+    'nav.projects': 'Projetos',
+    'nav.contact': 'Contato',
+    'about.area': 'Desenvolvedor Full-Stack WEB',
+    'about.about': 'Sobre',
+    'about.me': `Como Full-Stack, busco constantemente expandir meus conhecimentos e
+      habilidades, tenho experiência tanto em frontend quanto em backend, estou
+      familiarizado com tecnologias como: Angular, Spring Boot, Nextjs,
+      TypeScript, Node e NestJS, utilizando essas ferramentas para criar
+      soluções eficientes e escaláveis. Estou pronto para enfrentar desafios e
+      contribuir em qualquer equipe ou projeto em que me envolva.`,
+    'about.cv': 'Ver CV',
+  },
+  en: {
+    'site.title': 'Andrew Domingues',
+    'site.desc': 'Andrew Domingues portfolio',
+    'site.keywords':
+      'drewdomi, Drew Domi, Programming, Web Programming, Websites, Web Systems, Portuguese Red Cross, Red Cross of Amares',
+    'nav.home': 'Home',
+    'nav.projects': 'Projects',
+    'nav.contact': 'Contact',
+    'about.area': 'Full-Stack WEB Developer',
+    'about.about': 'About',
+    'about.me': `As a Full-Stack Developer, I constantly seek to expand my knowledge and
+      skills. I have experience in both frontend and backend development and am
+      familiar with technologies such as Angular, Spring Boot, Next.js,
+      TypeScript, Node.js, and NestJS. I use these tools to create efficient and
+      scalable solutions. I am ready to face challenges and contribute
+      with my knowledge and experience to any team or project.`,
+    'about.cv': 'My CV',
+  },
+} as const
